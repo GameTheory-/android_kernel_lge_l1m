05ba4643f36dfeeef8f24471ca62cea8c84dd1a6
GameTheory <gametheory@ubuntu.(none)>
2013-12-23 05:09:06 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Freedom Kernel l1m
+
+- 1.8ghz Overclock
+- Interactive Gov
+- Conservative Gov
+- SmartassV2 Gov
+- Superbad Gov
+- Intellidemand Gov
---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 9 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#9 SMP PREEMPT Sat Dec 21 21:49:03 EST 2013"
+#define LINUX_COMPILE_BY "gametheory"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
