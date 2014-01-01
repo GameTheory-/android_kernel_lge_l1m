72cb245d583324dae1920e41ec017b913081093c
GameTheory <gametheory@ubuntu.(none)>
2014-01-01 16:34:57 -0500

    update

--- gt.sh
+++ gt.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 
-# My personal build script. Mod it to your liking or delete if not needed.
+# My personal build script. Mod it to fit your needs or delete if not needed.
 
 export ARCH=arm
 export TARGET_PRODUCT=l1m_MPCS_US
-export CROSS_COMPILE=/home/gametheory/android/cm11/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
+export CROSS_COMPILE=/home/gametheory/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
 make ARCH=arm l1m-perf_defconfig
 make -j4
