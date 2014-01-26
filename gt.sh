19c649c4c45918c70b944b89a69565737de8de82
GameTheory- <jrtheory@gmail.com>
2014-01-26 01:30:14 -0500

    Delete gt.sh

--- gt.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-# My personal build script. Mod it to fit your needs or delete if not needed.
-
-export ARCH=arm
-export TARGET_PRODUCT=l1m_MPCS_US
-export CROSS_COMPILE=/home/gametheory/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
-make ARCH=arm l1m-perf_defconfig
-make -j4
